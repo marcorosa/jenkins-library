d842a906a4c0e39b29cde6201277026bed89c431
Marco Rosa <marcor165@hotmail.it>
2022-08-08 14:48:51 +0200

    Add mkdocs reference and metadata generated

--- cmd/metadata_generated.go
+++ cmd/metadata_generated.go
@@ -49,6 +49,7 @@ func GetAllStepMetadata() map[string]config.StepData {
 		"codeqlExecuteScan":                         codeqlExecuteScanMetadata(),
 		"containerExecuteStructureTests":            containerExecuteStructureTestsMetadata(),
 		"containerSaveImage":                        containerSaveImageMetadata(),
+		"credentialdiggerScanPullRequest":           credentialdiggerScanPullRequestMetadata(),
 		"detectExecuteScan":                         detectExecuteScanMetadata(),
 		"fortifyExecuteScan":                        fortifyExecuteScanMetadata(),
 		"gaugeExecuteTests":                         gaugeExecuteTestsMetadata(),
