65126bd1068383f4e029721f6a6254dc156a3ff3
Marco Rosa <marcor165@hotmail.it>
2022-09-27 11:14:57 +0200

    copy githubCommentIssue method for testing purposes

--- cmd/credentialdiggerTestStep.go
+++ cmd/credentialdiggerTestStep.go
@@ -1,78 +1,49 @@
 package cmd
 
 import (
-	"fmt"
-	"github.com/SAP/jenkins-library/pkg/command"
+	"context"
+	"strings"
+
 	"github.com/SAP/jenkins-library/pkg/log"
-	"github.com/SAP/jenkins-library/pkg/piperutils"
 	"github.com/SAP/jenkins-library/pkg/telemetry"
+	"github.com/google/go-github/v45/github"
+	"github.com/pkg/errors"
+
+	piperGithub "github.com/SAP/jenkins-library/pkg/github"
 )
 
-type credentialdiggerTestStepUtils interface {
-	command.ExecRunner
-
-	FileExists(filename string) (bool, error)
-
-	// Add more methods here, or embed additional interfaces, or remove/replace as required.
-	// The credentialdiggerTestStepUtils interface should be descriptive of your runtime dependencies,
-	// i.e. include everything you need to be able to mock in tests.
-	// Unit tests shall be executable in parallel (not depend on global state), and don't (re-)test dependencies.
-}
-
-type credentialdiggerTestStepUtilsBundle struct {
-	*command.Command
-	*piperutils.Files
-
-	// Embed more structs as necessary to implement methods or interfaces you add to credentialdiggerTestStepUtils.
-	// Structs embedded in this way must each have a unique set of methods attached.
-	// If there is no struct which implements the method you need, attach the method to
-	// credentialdiggerTestStepUtilsBundle and forward to the implementation of the dependency.
-}
-
-func newCredentialdiggerTestStepUtils() credentialdiggerTestStepUtils {
-	utils := credentialdiggerTestStepUtilsBundle{
-		Command: &command.Command{},
-		Files:   &piperutils.Files{},
-	}
-	// Reroute command output to logging framework
-	utils.Stdout(log.Writer())
-	utils.Stderr(log.Writer())
-	return &utils
+type credentialdiggerTestStepService interface {
+	// TODO: get owner and repo from repository string
+	CreateComment(ctx context.Context, owner string, repo string, number int, comment *github.IssueComment) (*github.IssueComment, *github.Response, error)
 }
 
 func credentialdiggerTestStep(config credentialdiggerTestStepOptions, telemetryData *telemetry.CustomData) {
-	// Utils can be used wherever the command.ExecRunner interface is expected.
-	// It can also be used for example as a mavenExecRunner.
-	utils := newCredentialdiggerTestStepUtils()
-
-	// For HTTP calls import  piperhttp "github.com/SAP/jenkins-library/pkg/http"
-	// and use a  &piperhttp.Client{} in a custom system
-	// Example: step checkmarxExecuteScan.go
-
-	// Error situations should be bubbled up until they reach the line below which will then stop execution
-	// through the log.Entry().Fatal() call leading to an os.Exit(1) in the end.
-	err := runCredentialdiggerTestStep(&config, telemetryData, utils)
+	ctx, client, err := piperGithub.NewClient(config.Token, config.APIURL, "", []string{})
 	if err != nil {
-		log.Entry().WithError(err).Fatal("step execution failed")
+		log.Entry().WithError(err).Fatal("Failed to get GitHub client")
+	}
+	err = runCredentialdiggerTestStep(ctx, &config, telemetryData, client.Issues)
+	if err != nil {
+		log.Entry().WithError(err).Fatal("Failed to comment on issue")
 	}
 }
 
-func runCredentialdiggerTestStep(config *credentialdiggerTestStepOptions, telemetryData *telemetry.CustomData, utils credentialdiggerTestStepUtils) error {
-	log.Entry().WithField("LogField", "Log field content").Info("This is just a demo for a simple step.")
+func runCredentialdiggerTestStep(ctx context.Context, config *credentialdiggerTestStepOptions, telemetryData *telemetry.CustomData, service credentialdiggerTestStepService) error {
 
-	// Example of calling methods from external dependencies directly on utils:
-	exists, err := utils.FileExists("file.txt")
+	issueComment := github.IssueComment{
+		Body: &config.Body,
+	}
+
+	// TODO get Repo name and Owner from repo url
+	s := strings.Split(config.Repository, "/")
+	owner := s[len(s)-2]
+	repoName := s[len(s)-1]
+	newcomment, resp, err := service.CreateComment(ctx, owner, repoName, config.Number, &issueComment)
 	if err != nil {
-		// It is good practice to set an error category.
-		// Most likely you want to do this at the place where enough context is known.
-		log.SetErrorCategory(log.ErrorConfiguration)
-		// Always wrap non-descriptive errors to enrich them with context for when they appear in the log:
-		return fmt.Errorf("failed to check for important file: %w", err)
+		log.Entry().Errorf("GitHub response code %v", resp.Status)
+		return errors.Wrapf(err, "Error occurred when creating comment on issue %v", config.Number)
 	}
-	if !exists {
-		log.SetErrorCategory(log.ErrorConfiguration)
-		return fmt.Errorf("cannot run without important file")
-	}
+	log.Entry().Debugf("New issue comment created for issue %v: %v", config.Number, newcomment)
 
 	return nil
 }
