db93ac5af112f1d3cb3d9187c3658061fca7f9e9
Marco Rosa <marco.rosa@sap.com>
2022-10-06 16:49:41 +0200

    Add placeholders for tests

--- cmd/credentialdiggerScan_test.go
+++ cmd/credentialdiggerScan_test.go
@@ -1,9 +1,10 @@
 package cmd
 
 import (
+	"testing"
+
 	"github.com/SAP/jenkins-library/pkg/mock"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 type credentialdiggerScanMockUtils struct {
@@ -11,7 +12,7 @@ type credentialdiggerScanMockUtils struct {
 	*mock.FilesMock
 }
 
-func newCredentialdiggerScanTestsUtils() credentialdiggerScanMockUtils {
+func newCDTestsUtils() credentialdiggerScanMockUtils {
 	utils := credentialdiggerScanMockUtils{
 		ExecMockRunner: &mock.ExecMockRunner{},
 		FilesMock:      &mock.FilesMock{},
@@ -19,6 +20,80 @@ func newCredentialdiggerScanTestsUtils() credentialdiggerScanMockUtils {
 	return utils
 }
 
+func TestCredentialdiggerFullScan(t *testing.T) {
+	t.Run("Valid full scan with discoveries", func(t *testing.T) {
+		// TODO
+	})
+	t.Run("Valid full scan without discoveries", func(t *testing.T) {
+		// TODO
+	})
+
+	t.Run("Invalid repository", func(t *testing.T) {
+		// TODO
+	})
+	t.Run("Invalid git token", func(t *testing.T) {
+		// TODO
+	})
+	t.Run("Invalid API endpoint", func(t *testing.T) {
+		// TODO
+	})
+	t.Run("Invalid ML models", func(t *testing.T) {
+		// TODO
+	})
+}
+
+func TestCredentialdiggerScanSnapshot(t *testing.T) {
+	t.Run("Valid scan snapshot with discoveries", func(t *testing.T) {
+		// TODO
+	})
+	t.Run("Valid scan snapshot without discoveries", func(t *testing.T) {
+		// TODO
+	})
+	t.Run("Invalid snapshot in scan snapshot", func(t *testing.T) {
+		// TODO
+	})
+}
+
+func TestCredentialdiggerScanPR(t *testing.T) {
+	t.Run("Valid scan pull request with discoveries", func(t *testing.T) {
+		// TODO
+	})
+	t.Run("Valid scan pull request without discoveries", func(t *testing.T) {
+		// TODO
+	})
+	t.Run("Invalid pr number in scan pull request", func(t *testing.T) {
+		// TODO
+	})
+}
+
+func TestCredentialdiggerAddRules(t *testing.T) {
+	t.Run("Valid standard rules", func(t *testing.T) {
+		config := credentialdiggerScanOptions{}
+		utils := newCDTestsUtils()
+		assert.Equal(t, nil, credentialdiggerAddRules(&config, nil, utils))
+	})
+	t.Run("Valid external rules", func(t *testing.T) {
+		rulesExt := "https://raw.githubusercontent.com/SAP/credential-digger/main/ui/backend/rules.yml"
+		config := credentialdiggerScanOptions{RulesDownloadURL: rulesExt}
+		utils := newCDTestsUtils()
+		assert.Equal(t, nil, credentialdiggerAddRules(&config, nil, utils))
+	})
+	t.Run("Invalid external rules link", func(t *testing.T) {
+		rulesExt := "https://broken-link.com/fakerules"
+		config := credentialdiggerScanOptions{RulesDownloadURL: rulesExt}
+		utils := newCDTestsUtils()
+		assert.Equal(t, nil, credentialdiggerAddRules(&config, nil, utils))
+	})
+	t.Run("Invalid external rules file format", func(t *testing.T) {
+		rulesExt := "https://raw.githubusercontent.com/SAP/credential-digger/main/requirements.txt"
+		config := credentialdiggerScanOptions{RulesDownloadURL: rulesExt}
+		utils := newCDTestsUtils()
+		assert.Equal(t, nil, credentialdiggerAddRules(&config, nil, utils))
+	})
+
+}
+
+/*
 func TestRunCredentialdiggerScan(t *testing.T) {
 	t.Parallel()
 
@@ -27,7 +102,7 @@ func TestRunCredentialdiggerScan(t *testing.T) {
 		// init
 		config := credentialdiggerScanOptions{}
 
-		utils := newCredentialdiggerScanTestsUtils()
+		utils := newCDTestsUtils()
 		utils.AddFile("file.txt", []byte("dummy content"))
 
 		// test
@@ -49,5 +124,16 @@ func TestRunCredentialdiggerScan(t *testing.T) {
 
 		// assert
 		assert.EqualError(t, err, "cannot run without important file")
+	})
+
+}
+*/
+
+func TestCredentialdiggerGetDiscoveries(t *testing.T) {
+	t.Run("Valid get discoveries", func(t *testing.T) {
+		// TODO
+	})
+	t.Run("Empty discoveries", func(t *testing.T) {
+		// TODO
 	})
 }
