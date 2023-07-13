d0f8b6b66cb18cfc52e861323e3bd0f44e05503b
Marco Rosa <marco.rosa@sap.com>
2023-07-13 16:13:14 +0200

    Return error code -1

--- cmd/credentialdiggerScan.go
+++ cmd/credentialdiggerScan.go
@@ -49,15 +49,15 @@ func credentialdiggerScan(config credentialdiggerScanOptions, telemetryData *tel
 	}
 	if config.Repository == "" {
 		// Get current repository from orchestrator
-		log.Entry().Debug("Repository URL not defined in step configuration. Try get it from orchestrators")
+		log.Entry().Debug("Repository URL not defined in step configuration. Get it from orchestrator")
 		repoUrlOrchestrator := provider.GetRepoURL()
 		if repoUrlOrchestrator == "n/a" {
 			// Jenkins configuration error
 			configError := errors.New(fmt.Sprintf("Unknown repository URL %s", repoUrlOrchestrator))
 			log.Entry().WithError(configError).Error(
-				"Repository URL n/a. Please verify git plugin is installed.")
+				"Repository URL unknown. Please verify git plugin is installed.")
 			// The repository to scan was not identified. Return an error
-			return configError
+			return -1
 		}
 		config.Repository = repoUrlOrchestrator
 		log.Entry().Debug("Use current repository: ", repoUrlOrchestrator)
